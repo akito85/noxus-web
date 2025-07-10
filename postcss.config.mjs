2ee800a0d93a0aca09109942f81f382429f2ba43
Vitrio Wibisono <vitrio@noxus.co.id>
2025-07-10 18:13:32 +0700

    Added basic config fonts

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,13 @@
 const config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+      },
+    },
+  },
   plugins: ["@tailwindcss/postcss"],
-};
+}
 
 export default config;
